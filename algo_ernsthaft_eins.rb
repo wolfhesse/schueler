11ae13d8cac9c00d7761b69447784bba7a25cda9
johannas <joschoeggl@gmail.com>
2014-02-16 20:20:57 +0100

    wp mit button 3

--- algo_ernsthaft_eins.rb
+++ algo_ernsthaft_eins.rb
@@ -6,9 +6,13 @@ class AlgoErnsthaftEins
 
   def next_xy
     x=@voriges_x+@step
-    y=Math::E
+    y=Math::E ** x
     @voriges_x =x
 
     return x, y
+  end
+
+  def reset2
+    @voriges_x =0
   end
 end
