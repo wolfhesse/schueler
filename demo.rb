ce8f25ea7873695de9ee4f973a6b247f60723abf
rogera <wolfgang.schuessel@gmail.com>
2014-02-17 18:46:24 +0100

    demo class refactoring

--- demo.rb
+++ demo.rb
@@ -11,16 +11,23 @@ require './echte_services'
 require './zeichenservice'
 require './zeichenflaeche_dummy'
 
+class Demo
+  def initialize
 
-zeichenflaeche = ZeichenflaecheDummy.new
+    zeichenflaeche = ZeichenflaecheDummy.new
 # zeichenservice mit zeichenflaeche_dummy und algo_dummy als Service-1
 
-zeichenservice = Zeichenservice.new(zeichenflaeche, AlgoErnsthaftEins.new)
+    zeichenservice = Zeichenservice.new(zeichenflaeche, AlgoErnsthaftEins.new)
 
-laufzeitsystem = LaufzeitSystem.new(zeichenservice)
+    laufzeitsystem = LaufzeitSystem.new(zeichenservice)
 
 # dem initial_setup die zeichenflaeche einhaengen
-laufzeitsystem.initial_setup(zeichenflaeche)
+    laufzeitsystem.initial_setup(zeichenflaeche)
 
-laufzeitsystem.start
+    laufzeitsystem.start
 
+  end
+end
+
+
+Demo.new
